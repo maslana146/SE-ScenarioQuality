3854ea3717776b84604508de4885314274d01965
radoslawbodus <80386785+radoslawbodus@users.noreply.github.com>
2021-05-24 10:56:05 +0200

    Update SimplifiedRequirementsObtainer.java

--- src/main/java/pl/put/poznan/sqc/logic/analysis/SimplifiedRequirementsObtainer.java
+++ src/main/java/pl/put/poznan/sqc/logic/analysis/SimplifiedRequirementsObtainer.java
@@ -1,10 +1,51 @@
 package pl.put.poznan.sqc.logic.analysis;
 
 import pl.put.poznan.sqc.model.Scenario;
+import pl.put.poznan.sqc.model.Step;
+import java.util.ArrayList;
 
 public class SimplifiedRequirementsObtainer implements ScenarioVisitor {
-    @Override
-    public Integer visitScenario(Scenario scenario) {
-        return 5;
+
+    private ArrayList<Step> steps = new ArrayList<Step>();
+    private int maxLevel;
+
+    public SimplifiedRequirementsObtainer(int maxLevel){
+        this.maxLevel = maxLevel;
     }
+
+    @Override
+    public Scenario visitScenario(Scenario scenario) {
+        this.getStepsLevel(scenario);
+
+        Scenario scenario1 = new Scenario();
+
+        scenario1.setHeader(scenario.getHeader());
+        scenario1.setSteps(this.steps);
+
+        return scenario1;
+
+    }
+
+    public ArrayList<Step> getStepsLevel(Scenario scenario) {
+        if (maxLevel < 1 && scenario.getSteps() == null) {
+            return null;
+        }
+
+        if (maxLevel > 0)
+        for (Step step: scenario.getSteps()) {
+            diveIntoStep(step, 1, maxLevel);
+        }
+
+        return steps;
+    }
+
+    public void diveIntoStep(Step step, int currentLevel, int maxLevel) {
+        steps.add(step);
+        if (step.getSteps() != null && currentLevel < maxLevel) {
+            for (Step newStep : step.getSteps()) {
+                diveIntoStep(newStep, currentLevel + 1, maxLevel);
+            }
+        }
+    }
+
 }
